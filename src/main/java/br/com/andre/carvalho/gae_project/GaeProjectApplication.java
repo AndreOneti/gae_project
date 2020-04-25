b25f1731aaf3be70e317ab93fcbb53cffb684508
AndreOneti <andreluiz.onete@hotmail.com>
2020-04-25 15:10:30 -0300

    first commit

---
+++ src/main/java/br/com/andre/carvalho/gae_project/GaeProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.carvalho.gae_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andre/carvalho/gae_project/GaeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.carvalho.gae_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
